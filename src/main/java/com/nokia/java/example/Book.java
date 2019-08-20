0b0933d361019a65c0d8194d056be46505cec698
Toan Nguyen Khanh <nktoan@tma.com.vn>
2019-08-20 14:43:44 +0700

    first commit

--- src/main/java/com/nokia/java/example/Book.java
+++ src/main/java/com/nokia/java/example/Book.java
@@ -9,4 +9,19 @@ class Book implements Comparable {
         Book book = (Book) b;
         return (title.compareTo(book.title));
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                '}';
+    }
 }
