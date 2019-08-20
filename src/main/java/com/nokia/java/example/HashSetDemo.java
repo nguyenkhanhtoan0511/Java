6743eef712becacc45015b2c360c25e5e1902a61
Toan Nguyen Khanh <nktoan@tma.com.vn>
2019-08-20 14:32:19 +0700

    first commit

---
+++ src/main/java/com/nokia/java/example/HashSetDemo.java
@@ -0,0 +1,15 @@
+package com.nokia.java.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class HashSetDemo {
+
+    public static void main(String[] args) {
+        List list = Arrays.asList("one Two three Four five six one three Four".split(" "));
+        System.out.println(list);
+        System.out.println("max: " + Collections.max(list));
+        System.out.println("min: " + Collections.min(list));
+    }
+}
---
+++ src/main/java/com/nokia/java/example/LinkedListDemo.java
@@ -0,0 +1,38 @@
+package com.nokia.java.example;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String args[]) {
+        // tao mot linked list
+        LinkedList ll = new LinkedList();
+        // them cac phan tu toi linked list
+        ll.add("F");
+        ll.add("B");
+        ll.add("D");
+        ll.add("E");
+        ll.add("C");
+        ll.addLast("Z");
+        ll.addFirst("A");
+        ll.add(1, "A2");
+        System.out.println("Noi dung ban dau cua LinkedList: " + ll);
+
+        // xoa cac phan tu tu linked list
+        ll.remove("F");
+        ll.remove(2);
+        System.out.println("Noi dung cua LinkedList sau khi xoa: "
+                + ll);
+
+        // xoa cac phan tu dau tien va cuoi cung
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println("LinkedList sau khi xoa phan tu dau tien va cuoi cung: "
+                + ll);
+
+        // lay va thiet lap mot gia tri
+        Object val = ll.get(2);
+        ll.set(2, (String) val + " Dathaydoi");
+        System.out.println("LinkedList sau khi thay doi: " + ll);
+    }
+}
